d95669305756fe3497cae22e13156eb6baeee90c
chaerin <100216331+julia-heo@users.noreply.github.com>
2024-09-28 12:27:33 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.13 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,46 +1,42 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <algorithm>
+
 using namespace std;
 
 int solution(vector<string> friends, vector<string> gifts) {
-    int answer = 0;
+    int answer = 0,index;
+    int size=friends.size();
+    map <string, int> m;
+    vector<vector<int>> his(size,vector<int>(size,0));
+    vector<int> score(size,0);
     
-    int fNum=friends.size();
-    map<string,int> m;
-    m.clear();
-    for(int i=0;i<fNum;i++){
+    for(int i=0;i<size;i++){
         m.insert(make_pair(friends[i],i));
+    }    
+    for(string str : gifts){
+        index=str.find(' ');
+        string fromN = str.substr(0,index);
+        string toN = str.substr(index+1);
+        int from=m[fromN];
+        int to=m[toN];
+        his[from][to]++;
+        score[from]++;
+        score[to]--;
     }
-    
-    vector <vector<int>> vec(fNum,vector<int>(fNum,0));
-    vector <int> jisu(fNum,0);
-    
-    for(int i=0;i<gifts.size();i++){
-        string str=gifts[i];
-        int index=str.find(' ');
-        string f1 = str.substr(0,index); 
-        string f2 = str.substr(index+1);
-        int f1num=m[f1];
-        int f2num=m[f2];
-        
-        
-        vec[f1num][f2num]+=1;
-        jisu[f1num]+=1;
-        jisu[f2num]-=1;
-        
-    }
-    for(int i=0;i<fNum;i++){
-        int sun=0;
-        for(int j=0;j<fNum;j++){
-            if(vec[i][j]>vec[j][i])sun++;
-            else if(vec[i][j]==vec[j][i]){
-                if(jisu[i]>jisu[j])sun++;
-            }    
+    vector<int> giftCount(size,0);
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            if(his[i][j]>his[j][i]) giftCount[i]++;
+            else if(his[i][j]==his[j][i]){
+                if(score[i]>score[j])giftCount[i]++;
+            }
         }
-        if(sun>answer)answer=sun;
     }
+    for(int i=0;i<size;i++){
+        if(giftCount[i]>answer)answer=giftCount[i];
+    }   
+    
     
     return answer;
 }
