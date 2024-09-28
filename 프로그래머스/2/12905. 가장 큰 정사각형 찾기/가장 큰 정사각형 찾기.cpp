c4ee404c67a360bbd0f40b298178ae22c1604252
chaerin <100216331+julia-heo@users.noreply.github.com>
2024-09-28 13:17:55 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 6.77 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -9,20 +9,21 @@ int solution(vector<vector<int>> board)
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     cout << "Hello Cpp" << endl;
     
-    int size1 = board.size();
-    int size2 = board[0].size();
-    vector<vector<int>> dp(size1,vector<int>(size2,0));
+    int col=board.size();
+    int row=board[0].size();
+    vector<vector<int>> dp (col,vector<int>(row,0));
     
-    for(int i=0;i<size1;i++){dp[i][0]=board[i][0];if(dp[i][0]>answer)answer=dp[i][0];}
-    for(int i=0;i<size2;i++){dp[0][i]=board[0][i];if(dp[0][i]>answer)answer=dp[0][i];}
     
-    for(int i=1;i<size1;i++){
-        for(int j=1;j<size2;j++){
-            if(board[i][j]==0)dp[i][j]=0;
-            else{dp[i][j]=min(min(dp[i-1][j-1],dp[i][j-1]),dp[i-1][j])+1;}
+    for(int i=0;i<col;i++){
+        for(int j=0;j<row;j++){
+            if(i==0||j==0)dp[i][j]=board[i][j];
+            else if (board[i][j]==0)dp[i][j]=0;
+            else{
+                dp[i][j]=min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]))+1;
+            }
             if(dp[i][j]>answer)answer=dp[i][j];
         }
-    }    
-    answer=answer*answer;
-    return answer;
+    }
+
+    return answer*answer;
 }
